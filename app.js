ff758a9f63ce4eb455d3cf225723ccec378271c3
Xavier <xavierpriceau@gmail.com>
2023-08-31 10:00:33 +1000

    add winning message js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATION = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
+const winningMessageElement = document.getElementById("winningMessage");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
 let circleTurn;
 
 startGame();
@@ -19,11 +33,22 @@ function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  }
   //check for win
   //check for draw
   //switch turns
   switchTurns();
   setBoardHoverClass();
+}
+
+function endGame(draw) {
+  if (draw) {
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessageElement.classList.add("show");
 }
 
 function placeMark(cell, currentClass) {
@@ -42,4 +67,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS);
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATION.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
