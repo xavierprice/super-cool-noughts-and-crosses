47cdcca928af4c76169c549cbe302626b041da8d
Xavier <xavierpriceau@gmail.com>
2023-09-13 17:42:23 +1000

    rearrange js for better workflow

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
 const LARGE_X_CLASS = "large-x";
 const LARGE_CIRCLE_CLASS = "large-circle";
+const dataBoard = document.querySelectorAll("[data-board]");
+const cellElements = document.querySelectorAll("[data-cell]");
+const boardElements = document.querySelectorAll(".board");
+const winningMessageElement = document.getElementById("winningMessage");
+const restartButton = document.getElementById("restartButton");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+let circleTurn;
 const LARGE_CELL_WINNING_COMBINATION = [
   [0, 1, 2],
   [3, 4, 5],
@@ -14,7 +23,6 @@ const LARGE_CELL_WINNING_COMBINATION = [
 ];
 
 const WINNING_COMBINATION = [];
-
 for (let i = 0; i < 81; i += 9) {
   //horizontal & vertical
   for (let j = 0; j < 3; j++) {
@@ -34,20 +42,65 @@ for (let i = 0; i < 81; i += 9) {
   }
 }
 
-const dataBoard = document.querySelectorAll("[data-board]");
-const cellElements = document.querySelectorAll("[data-cell]");
-const boardElements = document.querySelectorAll(".board");
-const winningMessageElement = document.getElementById("winningMessage");
-const restartButton = document.getElementById("restartButton");
-const winningMessageTextElement = document.querySelector(
-  "[data-winning-message-text]"
-);
-let circleTurn;
-
 //function declarations
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
+}
+
+function switchTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+  boardElements.forEach((board) => {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+      board.classList.add(CIRCLE_CLASS);
+    } else {
+      board.classList.add(X_CLASS);
+    }
+  });
+}
+
+function continueGame() {
+  switchTurns();
+  cellElements.forEach((cell) => {
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.classList.remove("show");
+}
+
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return (
+      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    );
+  });
+}
+
+function isMinigameDraw(board) {
+  const cellsInBoard = board.querySelectorAll(".cell");
+  for (const cell of cellsInBoard) {
+    if (
+      !cell.classList.contains(CIRCLE_CLASS) &&
+      !cell.classList.contains(X_CLASS)
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function clearBoardClasses(board) {
+  const cellsInBoard = board.querySelectorAll(".cell");
+  cellsInBoard.forEach((cell) => {
+    cell.classList.remove(CIRCLE_CLASS, X_CLASS);
+  });
+  continueGame();
 }
 
 function checkMinigameWin(currentClass) {
@@ -58,12 +111,23 @@ function checkMinigameWin(currentClass) {
   });
 }
 
-function isDraw() {
-  return [...cellElements].every((cell) => {
-    return (
-      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-    );
+function checkWin(currentLargeClass) {
+  return LARGE_CELL_WINNING_COMBINATION.some((combination) => {
+    return combination.every((index) => {
+      return dataBoard[index].classList.contains(currentLargeClass);
+    });
   });
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "Draw!";
+  } else {
+    winningMessageTextElement.innerText = `${
+      circleTurn ? "Circle" : "Cross"
+    } Wins!`;
+  }
+  winningMessageElement.classList.add("show");
 }
 
 function endMinigame(draw) {
@@ -95,41 +159,6 @@ function endMinigame(draw) {
   }
 }
 
-function switchTurns() {
-  circleTurn = !circleTurn;
-}
-
-function setBoardHoverClass() {
-  boardElements.forEach((board) => {
-    board.classList.remove(X_CLASS);
-    board.classList.remove(CIRCLE_CLASS);
-    if (circleTurn) {
-      board.classList.add(CIRCLE_CLASS);
-    } else {
-      board.classList.add(X_CLASS);
-    }
-  });
-}
-
-function checkWin(currentLargeClass) {
-  return LARGE_CELL_WINNING_COMBINATION.some((combination) => {
-    return combination.every((index) => {
-      return dataBoard[index].classList.contains(currentLargeClass);
-    });
-  });
-}
-
-function endGame(draw) {
-  if (draw) {
-    winningMessageTextElement.innerText = "Draw!";
-  } else {
-    winningMessageTextElement.innerText = `${
-      circleTurn ? "Circle" : "Cross"
-    } Wins!`;
-  }
-  winningMessageElement.classList.add("show");
-}
-
 //end of function declarations
 
 startGame();
@@ -147,16 +176,6 @@ function startGame() {
     cell.classList.remove(X_CLASS);
     cell.classList.remove(CIRCLE_CLASS);
     cell.classList.add("cell");
-    cell.removeEventListener("click", handleClick);
-    cell.addEventListener("click", handleClick, { once: true });
-  });
-  setBoardHoverClass();
-  winningMessageElement.classList.remove("show");
-}
-
-function continueGame() {
-  switchTurns();
-  cellElements.forEach((cell) => {
     cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
   });
@@ -205,25 +224,4 @@ function handleClick(e) {
     switchTurns();
     setBoardHoverClass();
   }
-}
-
-function isMinigameDraw(board) {
-  const cellsInBoard = board.querySelectorAll(".cell");
-  for (const cell of cellsInBoard) {
-    if (
-      !cell.classList.contains(CIRCLE_CLASS) &&
-      !cell.classList.contains(X_CLASS)
-    ) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function clearBoardClasses(board) {
-  const cellsInBoard = board.querySelectorAll(".cell");
-  cellsInBoard.forEach((cell) => {
-    cell.classList.remove(CIRCLE_CLASS, X_CLASS);
-  });
-  continueGame();
 }
